c17d6e873ad3dbedadf7ffa7175ea602c3e801d5
Marcolino5 <102527399+Marcolino5@users.noreply.github.com>
2026-01-07 23:40:57 -0300

    Old data able to be gotten

--- template_docs/ivr_file_template.py
+++ template_docs/ivr_file_template.py
@@ -53,9 +53,9 @@ TOTAL_HEADER = '''
 \\section{Resumo Total}
 \\begin{longtable}[c]{|p{1.7cm}|p{2.6cm}|p{2.6cm}|p{2.3cm}|}
 	\\caption{Resumo total} \\\\ \\hline
-	\\textbf{IVR (R\$)} &
-	\\textbf{Correção Monetária e Juros (R\$)} &
-	\\textbf{Total IVR (R\$)}
+	\\textbf{Diferença IVR} &
+	\\textbf{Atualização} &
+	\\textbf{Total devido}
 	\\endhead \\hline
 '''
 
@@ -68,9 +68,9 @@ YEAR_HEADER = '''
 \\begin{longtable}[c]{|p{1.7cm}|p{2.6cm}|p{2.6cm}|p{2.3cm}|}
 	\\caption{Resumo ano a ano} \\\\ \\hline
 	\\textbf{Ano} &
-	\\textbf{IVR (R\$)} &
-	\\textbf{Correção Monetária e Juros (R\$)} &
-	\\textbf{Total IVR (R\$)}
+	\\textbf{Diferença IVR} &
+	\\textbf{Atualização} &
+	\\textbf{Total devido}
 	\\endhead \\hline
 '''
 
@@ -82,10 +82,12 @@ MONTH_HEADER = '''
 \\section{Resumo mensal}
 \\begin{longtable}[c]{|p{1.7cm}|p{2.6cm}|p{2.6cm}|p{2.3cm}|p{2.3cm}|p{2.3cm}|c|}
 	\\caption{Resumo mês a mês} \\\\ \\hline
-	\\textbf{Mês/Ano} &
-	\\textbf{IVR (R\$)} &
-	\\textbf{Correção Monetária e Juros (R\$)} &
-	\\textbf{Total IVR (R\$)}
+	\\textbf{Mês} &
+	\\textbf{Valor original pago pelo SUS} &
+	\\textbf{Diferença IVR} &
+	\\textbf{Atualização antes de 01/2022} &
+	\\textbf{Selic a partir de 01/2022} &
+	\\textbf{Total devido}
 	\\endhead \\hline
 '''
 
